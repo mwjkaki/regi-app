65e937637118ba3b46fd552b1838a168ad8045c9
mwjkakiya <kakiya@mwjapan.jp>
2018-03-12 05:42:36 +0000

    make it better

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { OnsenModule } from 'ngx-onsenui';
 
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component
   ],
+  entryComponents: [Page1Component],
   imports: [
-    BrowserModule
+    BrowserModule,
+    OnsenModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
